6cc525ca29433984aba7a07e47693ab329246c65
Boominathan <eboominathan@gmail.com>
2024-12-02 07:17:02 +0530

    nabar & footer made from common

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const SITE_TITLE = 'Techpeople Blog';
+export const SITE_DESCRIPTION = 'Articles, Blogs & Tutorials from TechPeople';
+export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const ARTICLE_PER_PAGE = 6;
+
+
+
